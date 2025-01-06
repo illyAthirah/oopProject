4e332abda4429fdcb32e0f2461354d24469f92f2
illyathirah <86216356+illyAthirah@users.noreply.github.com>
2025-01-06 23:32:32 +0800

    Update Main.java

--- outpatientsystem/Main.java
+++ outpatientsystem/Main.java
@@ -31,7 +31,7 @@ public class Main {
             System.out.println("10. Exit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine(); 
 
             switch (choice) {
                 case 1:
